9ebc23e0934a1a14099107b90143ac7374f93d83
Gadzhi Kharkharov <me@kkga.me>
2024-05-09 18:54:41 +0300

    upd

--- app/deck.tsx
+++ app/deck.tsx
@@ -2,6 +2,7 @@
 
 import { useParams, useRouter } from 'next/navigation'
 import React, {
+  use,
   useCallback,
   useEffect,
   useRef,
@@ -18,32 +19,47 @@ interface Props {
 }
 
 export function Deck({ children }: Props) {
-  const [hash, setHash] = useState(getHash())
-  const params = useParams()
-  const router = useRouter()
-  const ref = useRef<HTMLDivElement>(null)
-  const slideCount = useRef<number>(0) // Use useRef to store slideCount
+  let [hash, setHash] = useState(getHash())
+  let [overviewMode, setOverviewMode] = useState(false)
+  let params = useParams()
+  let router = useRouter()
+  let ref = useRef<HTMLDivElement>(null)
+  let slideCount = useRef<number>(0) // Use useRef to store slideCount
 
-  const handlePrev = useCallback(() => {
-    const prevSlide = Math.max(1, parseInt(hash || '1', 10) - 1)
+  let handlePrev = useCallback(() => {
+    let prevSlide = Math.max(1, parseInt(hash || '1', 10) - 1)
     router.push(`#${prevSlide}`)
   }, [hash, router])
 
-  const handleNext = useCallback(() => {
-    const nextSlide = Math.min(
+  let handleNext = useCallback(() => {
+    let nextSlide = Math.min(
       slideCount.current,
       parseInt(hash || '1', 10) + 1
     )
     router.push(`#${nextSlide}`)
   }, [hash, router])
 
+  let handleOverviewToggle = useCallback(() => {
+    setOverviewMode((prev) => !prev)
+  }, [])
+
+  let handleSlideClick = useCallback(
+    (num: number) => {
+      if (overviewMode) {
+        router.push(`#${num + 1}`)
+        setOverviewMode(false)
+      }
+    },
+    [overviewMode, router]
+  )
+
   useEffect(() => {
     slideCount.current = ref.current?.children.length || 0
-    const currentHash = getHash() || '1'
+    let currentHash = getHash() || '1'
     setHash(currentHash)
 
-    const currentSlide = document.getElementById(currentHash)
-    const previousSlide = document.querySelector(
+    let currentSlide = document.getElementById(currentHash)
+    let previousSlide = document.querySelector(
       'section[data-current="true"]'
     )
     if (previousSlide && previousSlide !== currentSlide) {
@@ -52,21 +68,24 @@ export function Deck({ children }: Props) {
     if (currentSlide) {
       currentSlide.setAttribute('data-current', 'true')
     }
-  }, [hash, params, router, ref])
+    currentSlide?.scrollIntoView({
+      block: 'center',
+    })
+  }, [hash, params, router, ref, overviewMode])
 
   useEffect(() => {
-    const keyActions: Record<KeyboardEvent['key'], () => void> =
-      {
-        ArrowLeft: handlePrev,
-        ArrowUp: handlePrev,
-        k: handlePrev,
-        ArrowRight: handleNext,
-        ArrowDown: handleNext,
-        j: handleNext,
-      }
+    let keyActions: Record<KeyboardEvent['key'], () => void> = {
+      ArrowLeft: handlePrev,
+      ArrowUp: handlePrev,
+      k: handlePrev,
+      ArrowRight: handleNext,
+      ArrowDown: handleNext,
+      j: handleNext,
+      o: handleOverviewToggle,
+    }
 
-    const handleKeyDown = (event: KeyboardEvent) => {
-      const action = keyActions[event.key]
+    let handleKeyDown = (event: KeyboardEvent) => {
+      let action = keyActions[event.key]
       if (action) action()
     }
 
@@ -75,10 +94,33 @@ export function Deck({ children }: Props) {
     return () => {
       document.removeEventListener('keydown', handleKeyDown)
     }
-  }, [handleNext, handlePrev])
+  }, [handleNext, handleOverviewToggle, handlePrev])
+
+  useEffect(() => {
+    let slides = ref.current?.children
+
+    if (slides) {
+      const handlers = Array.from(slides).map((slide, i) => {
+        const handler = () => handleSlideClick(i)
+        slide.addEventListener('click', handler)
+        return handler
+      })
+
+      return () => {
+        Array.from(slides).forEach((slide, i) => {
+          slide.removeEventListener('click', handlers[i])
+        })
+      }
+    }
+  }, [handleSlideClick])
 
   return (
-    <div ref={ref} className="deck">
+    <div
+      ref={ref}
+      className={`deck ${
+        overviewMode === true ? 'overview' : ''
+      }`}
+    >
       {children}
     </div>
   )
